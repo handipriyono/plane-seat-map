1d092cd82e76f7b0e3035f70efbbf809a75e70f1
Handi Priyono <handimacbook@gmail.com>
2024-03-14 00:02:52 +0700

    feat: add commons component files

---
+++ src/commons/components/images.tsx
@@ -0,0 +1,25 @@
+export const ArrowDown = () => {
+  return (
+    <svg
+      width="15px"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 8.706 15.698"
+      id="down"
+    >
+      <path d="m8 10.646-3.147 3.14V0h-1v13.786l-3.147-3.14-.706.708 4.353 4.344 4.353-4.344z"></path>
+    </svg>
+  );
+};
+
+export const ArrowUp = () => {
+  return (
+    <svg
+      width="25px"
+      data-name="1-Arrow Up"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+    >
+      <path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z" />
+    </svg>
+  );
+};
