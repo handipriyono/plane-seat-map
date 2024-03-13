93ece73aa9208591180a8b0adf791b860e55bb40
Handi Priyono <handimacbook@gmail.com>
2024-03-14 01:19:25 +0700

    fix: adjust config vercel

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,14 @@ export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
     // vite config
+    plugins: [react()],
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
-      plugins: [react()],
+      VITE_API_GATEWAY: JSON.stringify(env.VITE_API_GATEWAY),
+      VITE_API_KEY_SUPABASE: JSON.stringify(env.VITE_API_KEY_SUPABASE),
     },
   };
 });
