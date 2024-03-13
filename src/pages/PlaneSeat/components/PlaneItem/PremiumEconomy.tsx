dcd31cb3642c87a6e72a0109cd68180658203c21
Handi Priyono <handimacbook@gmail.com>
2024-03-14 00:14:06 +0700

    feat: add premium Economy box

---
+++ src/pages/PlaneSeat/components/PlaneItem/PremiumEconomy.tsx
@@ -0,0 +1,69 @@
+import { TDataObjItem, TItem } from "../../types";
+import ItemBox from "./ItemBox";
+import usePlaneSeatStore from "../../stores/planeSeat.store";
+import ItemPath from "./ItemPath";
+
+const PremiumEconomy = ({ data }: TDataObjItem) => {
+  const selectedSeat = usePlaneSeatStore((state) => state.selected);
+  const lengthIndex = Object?.values(data)?.length;
+
+  return (
+    <>
+      <div className="title-class">Premium class</div>
+      <div className="flex-between">
+        <div className="label-red left">Exit</div>
+        <div className="label-red right">Exit</div>
+      </div>
+      <div>
+        {Object?.entries(data)?.map(([, itemList], idx) => {
+          const item = (x: TItem) => {
+            return {
+              item: x,
+              boxId: "box-premium-class",
+              seatNumber: x?.seat_number,
+              isOccupied: x?.occupied,
+              isActive: selectedSeat?.id === x?.id,
+            };
+          };
+
+          return (
+            <div key={itemList?.[0]?.id} style={{ position: "relative" }}>
+              <div className="box-number">{itemList?.[0]?.number}</div>
+
+              <div className="row-line">
+                <div className="line-wrap">
+                  <ItemBox {...item(itemList?.[0])} />
+                  <ItemBox {...item(itemList?.[1])} />
+                </div>
+
+                <ItemPath
+                  isFirst={idx === 0}
+                  isLast={lengthIndex === idx + 1}
+                />
+
+                <div className="line-wrap">
+                  <ItemBox {...item(itemList?.[2])} />
+                  <ItemBox {...item(itemList?.[3])} />
+                  <ItemBox {...item(itemList?.[4])} />
+                  <ItemBox {...item(itemList?.[5])} />
+                </div>
+
+                <ItemPath
+                  isFirst={idx === 0}
+                  isLast={lengthIndex === idx + 1}
+                />
+
+                <div className="line-wrap">
+                  <ItemBox {...item(itemList?.[6])} />
+                  <ItemBox {...item(itemList?.[7])} />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default PremiumEconomy;
